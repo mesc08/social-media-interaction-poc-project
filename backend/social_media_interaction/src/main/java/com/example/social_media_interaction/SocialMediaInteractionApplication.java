54cd9e8be982d6a0fad237d947687b457f0c7b36
Srijan Chakraborty <40038482+mesc08@users.noreply.github.com>
2023-07-07 00:57:38 +0530

    added changes for social media interaction

---
+++ backend/social_media_interaction/src/main/java/com/example/social_media_interaction/SocialMediaInteractionApplication.java
@@ -0,0 +1,13 @@
+package com.example.social_media_interaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaInteractionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaInteractionApplication.class, args);
+	}
+
+}
---
+++ backend/social_media_interaction/src/test/java/com/example/social_media_interaction/SocialMediaInteractionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.social_media_interaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaInteractionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
