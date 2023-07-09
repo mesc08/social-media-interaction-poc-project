7c8854095929c62198433f1b23165820002787a4
Srijan Chakraborty <40038482+mesc08@users.noreply.github.com>
2023-07-09 12:46:27 +0530

    added changes for connection models

---
+++ backend/social_media_interaction/src/main/java/com/example/social_media_interaction/entity/Connections.java
@@ -0,0 +1,8 @@
+package com.example.social_media_interaction.entity;
+
+import java.util.List;
+
+public abstract class Connections {
+    private Long Id;
+    private List<Long> UserLinked;
+}
---
+++ backend/social_media_interaction/src/main/java/com/example/social_media_interaction/entity/Followers.java
@@ -0,0 +1,6 @@
+package com.example.social_media_interaction.entity;
+
+
+public class Followers extends Connections{
+
+}
